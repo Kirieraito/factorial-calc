05f1f7fda826eab8d82317c86a681d60ee427207
Kirieraito <10943627+Kirieraito@users.noreply.github.com>
2020-03-30 00:13:52 +0300

    Added functionality with basic factorial algorithm

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import NumericInput from 'react-numeric-input';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class FactorialCalc extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            integer: 3,
+            result: 6,
+        }
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    render() {
+        return (
+        <div className="factorialCalc">
+            <div className="header">
+                <h1>Calculate the factorial of a positive integer</h1>
+            </div>
+            <div>
+                <NumericInput min={1} value={this.state.integer} class='numInput' onChange={this.updateResult} />
+                <h>{this.state.result}</h>
+            </div>
+        </div>
+        );
+    }
+    
+    
+    updateResult = (newInteger) => {
+        var newResult = factorial(newInteger);
+        this.setState({
+            integer: newInteger,
+            result: newResult,
+        })
+    }
+
+    
+
+}
+
+function factorial(n) {
+    if (n === 0) {
+        return 1;
+    }
+    else {
+        var result = 1;
+        for (let i=1; i<n+1; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
+
+ReactDOM.render(
+    <FactorialCalc />,
+    document.getElementById('root')
+  );
