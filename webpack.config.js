28a0d760775c625e20215d1935efb2c83e4a13c4
Kirieraito <10943627+Kirieraito@users.noreply.github.com>
2020-04-25 01:05:40 +0300

    Add resolve extensions to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,9 @@ module.exports = {
           }
         ]
     },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
 	plugins: [
         new HtmlWebpackPlugin({
           template: "./public/index.html"
